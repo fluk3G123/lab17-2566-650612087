3da310e853334d36a1fc8b420d66e1409d16198a
nithipong <nithipong_h@cmu.ac.th>
2023-09-09 12:55:48 +0700

    ok kub

--- src/app/api/enrollment/route.js
+++ src/app/api/enrollment/route.js
@@ -26,15 +26,23 @@ export const GET = async (request) => {
   }
 
   //check if user provide one of 'studentId' or 'courseNo'
-  //User must not provide both values, and must not provide nothing
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Please provide either studentId or courseNo and not both!",
-  //   },
-  //   { status: 400 }
-  // );
+  //User must not provide both values, and must not provide nothing NO1
+  if (studentId && courseNo)
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Please provide either studentId or courseNo and not both!",
+      },
+      { status: 400 }
+    );
+  if (!studentId && !courseNo)
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Please provide either studentId or courseNo and not both!",
+      },
+      { status: 400 }
+    );
 
   //get all courses enrolled by a student
   if (studentId) {
@@ -55,15 +63,22 @@ export const GET = async (request) => {
       ok: true,
       courses,
     });
-    //get all students enrolled by a course
+    //get all students enrolled by a course NO2
   } else if (courseNo) {
     const studentIdList = [];
     for (const enroll of DB.enrollments) {
       //your code here
+      if (enroll.courseNo === courseNo) {
+        studentIdList.push(enroll.studentId);
+      }
     }
 
     const students = [];
     //your code here
+    for (const studentId of studentIdList) {
+      const student = DB.students.find((x) => x.studentId === studentId);
+      students.push(student);
+    }
 
     return NextResponse.json({
       ok: true,
@@ -140,7 +155,19 @@ export const DELETE = async (request) => {
 
   const { studentId, courseNo } = body;
 
-  //check if studentId and courseNo exist on enrollment
+  //check if studentId and courseNo exist on enrollment NO3
+  const foundEnroll = DB.enrollments.find(
+    (x) => x.studentId === studentId && x.courseNo === courseNo
+  );
+  if (!foundEnroll) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Enrollment does not exist",
+      },
+      { status: 404 }
+    );
+  }
 
   // return NextResponse.json(
   //   {
@@ -151,6 +178,9 @@ export const DELETE = async (request) => {
   // );
 
   //perform deletion by using splice or array filter
+  DB.enrollments = DB.enrollments.filter(
+    (x) => x.studentId !== studentId || x.courseNo !== courseNo
+  );
 
   //if code reach here it means deletion is complete
   return NextResponse.json({
